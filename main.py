974c90751ed031c9ccfcea8470f09b5aff06c2af
ericflaub <91358141+ericflaub@users.noreply.github.com>
2023-06-19 19:20:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 import logging.handlers
 import os
 
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger_file_handler = logging.handlers.RotatingFileHandler(
