7263517f0605cac1b3aff6bb2d8000991d2395df
ericflaub <91358141+ericflaub@users.noreply.github.com>
2023-06-19 18:48:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ import logging
 import logging.handlers
 import os
 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
 #account = knowledge.account()
 try:
     token = os.environ["TOKEN"]
@@ -22,17 +35,7 @@ data = client.users.get_public_data('icererci')
 blitz_rating = data['perfs']['blitz']['rating']
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
+
 
 
 
